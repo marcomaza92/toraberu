f82b00086d0b7c76ca408e92557702daf885edd5
marcomaza92 <marcoagustinmaza@gmail.com>
2025-01-08 23:29:20 -0300

    feat: add appwrite in client side homepage

--- app/(main-layout)/(homepage)/page.tsx
+++ app/(main-layout)/(homepage)/page.tsx
@@ -1,6 +1,38 @@
-import styles from './page.module.css';
+"use client";
+
+import { useEffect } from "react";
+import styles from "./page.module.css";
+import { Client, Databases, Query } from "appwrite";
 
 const Homepage = () => {
+  const collections = {
+    places: "677f2d9a00065fe0c22b",
+  };
+
+  const fetchFromAppWrite = async () => {
+    const client = new Client()
+      .setEndpoint("https://cloud.appwrite.io/v1")
+      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
+
+    const databases = new Databases(client);
+
+    const dataAppWrite = databases
+      .listDocuments(
+        process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
+        collections.places,
+        [Query.select([])]
+      )
+      .then((data) => {
+        console.log(data);
+      });
+
+    console.log("appwrite", dataAppWrite);
+  };
+
+  useEffect(() => {
+    fetchFromAppWrite();
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.welcome}>
@@ -9,6 +41,6 @@ const Homepage = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Homepage;
