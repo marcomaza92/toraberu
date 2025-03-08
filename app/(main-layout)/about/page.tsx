b2ff233c147dd57962f7c427eafa6791d86df68a
marcomaza92 <marcoagustinmaza@gmail.com>
2025-03-07 22:25:23 -0300

    refactor: update about page styles'

--- app/(main-layout)/about/page.tsx
+++ app/(main-layout)/about/page.tsx
@@ -6,8 +6,10 @@ const About = () => {
       <h1>What&apos;s this?</h1>
 
       <section className={styles.section}>
-        <h2>All your beloved trips in one place</h2>
-        <p>
+        <h2 className={styles.sectionTitle}>
+          All your beloved trips in one place
+        </h2>
+        <p className={styles.sectionContent}>
           Plan and organize your travel destinations effortlessly. Save places
           you want to visit and schedule them for future trips. Never lose track
           of your travel wishlist again!
@@ -15,10 +17,10 @@ const About = () => {
       </section>
 
       <section className={styles.section}>
-        <h2>
+        <h2 className={styles.sectionTitle}>
           Powered with <em>some</em> AI
         </h2>
-        <p>
+        <p className={styles.sectionContent}>
           Search for any place worldwide using Google&apos;s extensive database
           and some AI systems. Get accurate location suggestions and save them
           to your personal collection with just a click.
@@ -26,8 +28,8 @@ const About = () => {
       </section>
 
       <section className={styles.section}>
-        <h2>Calendar connectivity on sight!</h2>
-        <p>
+        <h2 className={styles.sectionTitle}>Calendar connectivity on sight!</h2>
+        <p className={styles.sectionContent}>
           Organize your visits by date, group them automatically, and keep track
           of your travel schedule. Perfect for planning short trips or extended
           adventures.
