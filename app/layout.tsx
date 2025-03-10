90edd5d39c065ec49cf91c3c571d9ea7c56a541f
marcomaza92 <marcoagustinmaza@gmail.com>
2025-03-09 23:24:01 -0300

    fix: update layout's markup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,29 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Script from 'next/script'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Toraberu',
-  description: 'Toraberu - Trip Intelligent Assistant',
-}
+  title: "Toraberu",
+  description: "Toraberu - Trip Intelligent Assistant",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
-      <Script
-        defer
-        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
-      />
+      <body className={inter.className}>
+        <main>{children}</main>
+        <Script
+          defer
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
+        />
       </body>
-      
     </html>
-  )
+  );
 }
