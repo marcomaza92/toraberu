3323d7a905f9d5d87d21e4ec90eab57dd1f04c9a
marcomaza92 <marcoagustinmaza@gmail.com>
2025-04-14 23:49:44 -0300

    feat: update footer in layout file

--- app/(main-layout)/layout.tsx
+++ app/(main-layout)/layout.tsx
@@ -1,36 +1,24 @@
-import Link from 'next/link'
-import styles from './layout.module.css'
+import Link from "next/link";
+import styles from "./layout.module.css";
 
 export default function MainLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <div className={styles.layoutContainer}>
       <header className={styles.header}>
         <nav className={styles.nav}>
-          <Link href={`/`}>
-            Homepage
-          </Link>
-          <Link href={`/places-search`}>
-            Places Search
-          </Link>
-          <Link href={`/about`}>
-            What&apos;s this
-          </Link>
-          <Link href={`/login`}>
-            Login
-          </Link>
-          <Link href={`/register`}>
-            Register
-          </Link>
+          <Link href={`/`}>Homepage</Link>
+          <Link href={`/places-search`}>Places Search</Link>
+          <Link href={`/about`}>What&apos;s this</Link>
+          <Link href={`/login`}>Login</Link>
+          <Link href={`/register`}>Register</Link>
         </nav>
       </header>
-      <main className={styles.main}>
-        {children}
-      </main>
-      <footer className={styles.footer}>By marcomaza92</footer>
+      <main className={styles.main}>{children}</main>
+      <footer className={styles.footer}>Made with love by marcomaza92</footer>
     </div>
-  )
+  );
 }
